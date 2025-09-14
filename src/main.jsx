a7cb6aee49611f34ef0b822f9021083b90fecd1f
SKuytov <138648456+SKuytov@users.noreply.github.com>
2025-09-14 18:14:02 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+import '@/index.css';
 import { AuthProvider } from '@/contexts/SupabaseAuthContext';
-import { CurrencyProvider } from '@/contexts/CurrencyContext';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/lib/queryClient';
 import { Toaster } from '@/components/ui/toaster';
-
-import './index.css';
+import { CurrencyProvider } from '@/contexts/CurrencyContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <CurrencyProvider>
-          <App />
-          <Toaster />
-        </CurrencyProvider>
-      </AuthProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <CurrencyProvider>
+        <App />
+        <Toaster />
+      </CurrencyProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
