fd2180eab2c4153c823d9c10b24c1fa1587dfc5b
SKuytov <138648456+SKuytov@users.noreply.github.com>
2025-09-14 18:11:00 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import AppRouter from '@/router';
+import App from './App';
 import { AuthProvider } from '@/contexts/SupabaseAuthContext';
 import { CurrencyProvider } from '@/contexts/CurrencyContext';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <CurrencyProvider>
-          <AppRouter />
+          <App />
           <Toaster />
         </CurrencyProvider>
       </AuthProvider>
